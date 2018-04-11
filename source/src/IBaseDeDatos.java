a7f35a24734ca5ba8f7fade27d79d2a92b4c673b
Manuel López Reviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-04-11 23:19:36 +0200

    Update IBaseDeDatos.java

--- source/src/IBaseDeDatos.java
+++ source/src/IBaseDeDatos.java
@@ -1,64 +1,64 @@
 /*
   Historial:
-   - 11-04-2018: Creado por Manuel LÛpez Reviriego
+   - 11-04-2018: Creado por Manuel L√≥pez Reviriego
 */
 public interface IBaseDeDatos {
 	
 	/** Crea una cuenta en la base de datos, siempre y cuando el usuario no exista, y
-	 *  {@code usuario}, {@code password} y {@code email} sean v·lidos.
-	 *  @exception UsuarioExisteException (nombre no definitivo)
-	 *  @exception FormatoNoValidoException (nombre no definitivo)
-	 *  @author Manuel LÛpez Reviriego
-	 *  @date 11-04-2018
-	 *  @since 0.1
-	 */
+	*  {@code usuario}, {@code password} y {@code email} sean v√°lidos.
+	*  @exception UsuarioExisteException (nombre no definitivo)
+	*  @exception FormatoNoValidoException (nombre no definitivo)
+	*  @author Manuel L√≥pez Reviriego
+	*  @date 11-04-2018
+	*  @since 0.1
+	*/
 	void registrarCuenta(String usuario, String clave, String email);
 	
 	/** Elimina una cuenta en la base de datos, siempre y cuando el usuario exista y la clave
-	 *  sea correcta.
-	 *  @param campoEsEmail Ser· {@code true} si el {@code campo} representa un email. Si es {@code false}, entonces
-	 *  representar· un nombre de usuario.
-	 *  @exception CredencialesIncorrectasException (nombre no definitivo)
-	 *  @author Manuel LÛpez Reviriego
-	 *  @date 11-04-2018
-	 *  @since 0.1
-	 */
+	*  sea correcta.
+	*  @param campoEsEmail Ser√° {@code true} si el {@code campo} representa un email. Si es {@code false}, entonces
+	*  representar√° un nombre de usuario.
+	*  @exception CredencialesIncorrectasException (nombre no definitivo)
+	*  @author Manuel L√≥pez Reviriego
+	*  @date 11-04-2018
+	*  @since 0.1
+	*/
 	void eliminarCuenta(String campo, String clave, boolean campoEsEmail);
 	
-	/** Comprueba si el usuario est· registrado con dicha clave.
-	 *  @param campoEsEmail Ser· {@code true} si el {@code campo} representa un email. Si es {@code false}, entonces
-	 *  representar· un nombre de usuario.
-	 *  @exception UsuarioNoExisteException (nombre no definitivo)
-	 *  @exception EmailNoAsignadoException (nombre no definitivo)
-	 *  @author Manuel LÛpez Reviriego
-	 *  @date 11-04-2018
-	 *  @since 0.1
-	 */
+	/** Comprueba si el usuario est√° registrado con dicha clave.
+	*  @param campoEsEmail Ser√° {@code true} si el {@code campo} representa un email. Si es {@code false}, entonces
+	*  representar√° un nombre de usuario.
+	*  @exception UsuarioNoExisteException (nombre no definitivo)
+	*  @exception EmailNoAsignadoException (nombre no definitivo)
+	*  @author Manuel L√≥pez Reviriego
+	*  @date 11-04-2018
+	*  @since 0.1
+	*/
 	boolean verificarCredenciales(String campo, String clave, boolean campoEsEmail);
 	
 	/** Comprueba si existe un usuario que se ha registrado con el correo {@code email}.
-	 *  @author Manuel LÛpez Reviriego
-	 *  @date 11-04-2018
-	 *  @since 0.1
- 	 */
-     boolean existeEmail(String email);
+	*  @author Manuel L√≥pez Reviriego
+	*  @date 11-04-2018
+	*  @since 0.1
+	*/
+	boolean existeEmail(String email);
 
-    /** Si las credenciales son correctas, se asignar· un nuevo correo al usuario (reemplazando el anterior).
-     *  @exception CredencialesIncorrectasException (nombre no definitivo)
-     *  @exception FormatoNoValidoException (nombre no definitivo)
-   	 *  @author Manuel LÛpez Reviriego
-   	 *  @date 11-04-2018
-   	 *  @since 0.1
-     */
-     void cambiarEmail(String usuario, String clave, String nuevoEmail);
+        /** Si las credenciales son correctas, se asignar√° un nuevo correo al usuario (reemplazando el anterior).
+     	*  @exception CredencialesIncorrectasException (nombre no definitivo)
+     	*  @exception FormatoNoValidoException (nombre no definitivo)
+   	*  @author Manuel L√≥pez Reviriego
+   	*  @date 11-04-2018
+   	*  @since 0.1
+        */
+	void cambiarEmail(String usuario, String clave, String nuevoEmail);
 
-    /** Si las credenciales son correctas, se asignar· un nueva contraseÒa al usuario (reemplazando la anterior).
-     *  @exception CredencialesIncorrectasException (nombre no definitivo)
-     *  @exception FormatoNoValidoException (nombre no definitivo)
-  	 *  @author Manuel LÛpez Reviriego
-  	 *  @date 11-04-2018
-  	 *  @since 0.1
-   	 */
-	 void cambiarContraseÒa(String usuario, String clave, String nuevaClave);
-
+	/** Si las credenciales son correctas, se asignar√° un nueva contrase√±a al usuario (reemplazando la anterior).
+	*  @exception CredencialesIncorrectasException (nombre no definitivo)
+     	*  @exception FormatoNoValidoException (nombre no definitivo)
+	*  @author Manuel L√≥pez Reviriego
+	*  @date 11-04-2018
+	*  @since 0.1
+	*/
+	void cambiarContrase√±a(String usuario, String clave, String nuevaClave);
+	
 }
