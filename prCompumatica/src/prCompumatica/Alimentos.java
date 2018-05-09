cdc826f0725e645cdf0952d1fae3bfa9f4f76338
AntraxTeck <esnejider@gmail.com>
2018-05-09 14:02:08 +0200

    Update Alimentos.java

--- prCompumatica/src/prCompumatica/Alimentos.java
+++ prCompumatica/src/prCompumatica/Alimentos.java
@@ -1,5 +1,17 @@
 package prCompumatica;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Alimentos {
+  	public static String nombre= "pasta";
+	public static int gramos = 100;
+	public static int caloria100Gramos = 200;
+	public static int caloriasTotales = gramos * (caloria100Gramos/100);
+	List<String> nutrientes = Array.asList ("Proteinas", "Carbohidratos");
+	
 
+	assertThat(nombre, is("pasta"));
+	assertThat(gramos, is("100"));
+	assertThat(caloriasTotales, is("200"))
+	assertThat(nutrientes, contains("Proteinas", "Carbohidratos"));
 }
