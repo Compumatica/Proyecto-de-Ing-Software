4a240f332d7574da495a0af59ebcf044d527c4d8
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-09 13:38:08 +0200

    Add files via upload

--- prCompumatica/src/prCompumatica/Usuario.java
+++ prCompumatica/src/prCompumatica/Usuario.java
@@ -1,5 +1,35 @@
 package prCompumatica;
 
 public class Usuario {
-
+	private String usuario;
+	private String contraseña;
+	private String email;
+	
+	public Usuario(String usuario, String contraseña, String email){
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+		this.email = email;
+		Datos.RegistraUsuario(usuario, contraseña, email);
+	}
+	
+	public String consultarDatosPersonales() {
+		return Datos.datosPersonales(usuario);
+	}
+	
+	public String consultarDatosSobreDietas() {
+		return Datos.datosDietas(usuario);
+	}
+	
+	public String modificarDatos() {
+		return Datos.modificaDatosPersonales(usuario);
+	}
+	
+	public void darOpinion(String texto) {
+		Opinion op = new Opinion(texto);
+		Opinion.mandarOpinion(op);
+	}
+	
+	public String pedirDieta() {
+		return AsistenteDieta.hacerSugerencia(usuario);
+	}
 }
