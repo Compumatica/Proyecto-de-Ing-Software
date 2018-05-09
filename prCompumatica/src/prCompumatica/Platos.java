2a5696d17fd1aa0bedd4a1a56cd48ee17070fa7b
MiguelMJ <37369782+MiguelMJ@users.noreply.github.com>
2018-05-09 13:48:03 +0200

    Add files via upload

--- prCompumatica/src/prCompumatica/Platos.java
+++ prCompumatica/src/prCompumatica/Platos.java
@@ -1,5 +1,43 @@
 package prCompumatica;
 
-public class Platos {
+import java.util.ArrayList;
 
+public class Platos {
+	String nombre;
+	private ArrayList<Alimentos> componentes;
+	
+	
+	public Platos(String n, ArrayList<Alimentos> comp){
+		nombre=n;
+		componentes=comp;
+		
+	}
+	public Platos(String n) {
+		nombre=n;
+		componentes = new ArrayList<Alimentos>();
+	}
+	public Platos() {
+		this("plato_sin_nombre");
+	}
+	// modifiers
+	public void addAlimento(Alimentos a) {
+		componentes.add(a);
+	}
+	// getters
+	public ArrayList<Alimentos> componentes() {
+		ArrayList<Alimentos> clone = new ArrayList<Alimentos>(componentes);
+		return clone;
+	}
+	public String nombre() {
+		String ret = new String(nombre);
+		return ret;
+	}
+	// setters
+	public void nombre(String n) {
+		nombre=n;
+	}
+	public void componentes(ArrayList<Alimentos> comp) {
+		componentes=comp;
+	}
+	
 }
