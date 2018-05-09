6ee5977456a053a8f00d996ee2eb8f1abab28f6d
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-09 13:40:45 +0200

    Add files via upload

--- prCompumatica/src/prCompumatica/AsistenteDieta.java
+++ prCompumatica/src/prCompumatica/AsistenteDieta.java
@@ -2,4 +2,9 @@ package prCompumatica;
 
 public class AsistenteDieta {
 
+	public static String hacerSugerencia(String usuario) {
+		// La IA consultará la BD con el nombre de usuario para ver los datos del mismo y así saber que sugerencia hacer
+		return "";
+	}
+
 }
--- prCompumatica/src/prCompumatica/Datos.java
+++ prCompumatica/src/prCompumatica/Datos.java
@@ -2,4 +2,25 @@ package prCompumatica;
 
 public class Datos {
 
+	public static String modificaDatosPersonales(String usuario) {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	public static String datosDietas(String usuario) {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	public static String datosPersonales(String usuario) {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	public static void RegistraUsuario(String usuario, String contraseña, String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
