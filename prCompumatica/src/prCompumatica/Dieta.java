981869a898e33e9100f377257cf035068d17e5b4
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-15 12:12:34 +0200

    Clase Dieta.java
    Dieta, que consta de 4 platos.  Se dirige con el Enumerado de Comidas.java

--- prCompumatica/src/prCompumatica/Dieta.java
+++ prCompumatica/src/prCompumatica/Dieta.java
@@ -1,5 +1,36 @@
 package prCompumatica;
 
-public class Dieta {
+import java.util.*;
 
+public class Dieta {
+	// EnumMap <Clave, Valor>
+	// Clave es un objeto Enum de Comidas.java (desayuno, almuerzo, merienda, cena)
+	// Valor es un Plato
+	public EnumMap<Comidas, Platos> dieta;
+	
+	public Dieta() {
+		dieta =  new EnumMap<Comidas, Platos>(Comidas.class);
+	}
+	
+	public void agregaPlato(Platos p, Comidas c) {
+		
+		switch(c) {
+			case DESAYUNO:
+				dieta.put(Comidas.DESAYUNO, p);
+				break;
+			
+			case ALMUERZO:
+				dieta.put(Comidas.ALMUERZO, p);			
+				break;
+				
+			case MERIENDA:
+				dieta.put(Comidas.MERIENDA, p);
+				break;
+				
+			case CENA:
+				dieta.put(Comidas.CENA, p);
+				break;
+		}
+	}
+	
 }
