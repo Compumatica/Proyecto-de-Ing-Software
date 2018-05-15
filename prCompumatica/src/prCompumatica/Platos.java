9c8d39774143aaf733b75f90959bfa31c4792480
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-15 11:26:20 +0200

    Add files via upload
    Clase Platos modificada según lo acordado en la última reunión:
    * Añadidos atributos y getters de Proteínas, Grasas y Carbohidratos
    * Añadido atributo y getter de "franjaAlmuerzo". True indica que el plato es de Almuerzo o Cena. False indica que es de desayuno o merienda.

--- prCompumatica/src/prCompumatica/Platos.java
+++ prCompumatica/src/prCompumatica/Platos.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 public class Platos {
 	String nombre;
 	private ArrayList<Alimentos> componentes;
+	int Grasas, Carbohidratos, Proteinas;
 	
+	// True -> El plato es de almuerzo o cena 
+	// False -> El plato es de cena o merienda
+	boolean franjaAlmuerzo;
 	
 	public Platos(String n, ArrayList<Alimentos> comp){
 		nombre=n;
 		componentes=comp;
 		
 	}
+	
 	public Platos(String n) {
 		nombre=n;
 		componentes = new ArrayList<Alimentos>();
@@ -32,6 +37,26 @@ public class Platos {
 		String ret = new String(nombre);
 		return ret;
 	}
+	
+	// Respecto a franjaAlmuerzo (convenio)
+	// True -> El plato es de almuerzo o cena 
+	// False -> El plato es de cena o merienda
+	public boolean getHora() {
+		return franjaAlmuerzo;
+	}
+	
+	public int getGrasas() {
+		return Grasas;
+	}
+	
+	public int getCarbohidratos() {
+		return Carbohidratos;
+	}
+	
+	public int getProteinas() {
+		return Proteinas;
+	}
+	
 	// setters
 	public void nombre(String n) {
 		nombre=n;
