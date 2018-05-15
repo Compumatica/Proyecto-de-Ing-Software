54ee9ea7746efc9769b1b973d578180aea2d3937
nightroy99 <35336823+nightroy99@users.noreply.github.com>
2018-05-15 11:40:06 +0200

    Add files via upload

--- prCompumatica/src/prCompumatica/Opinion.java
+++ prCompumatica/src/prCompumatica/Opinion.java
@@ -1,5 +1,16 @@
 package prCompumatica;
 
 public class Opinion {
-
+	private String op;
+	
+	public Opinion(String op){
+		this.op = op;
+	}
+	String getOpinion() {
+		return op;
+	}
+	
+	public void mandarOpinion(Datos datos){
+		datos.recibirOpinion(this.op);
+	}
 }
