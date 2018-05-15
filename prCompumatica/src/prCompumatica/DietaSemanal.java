385bed64ce5b0dc6b965659ef5295a0f8a3ecdc9
Nestor Fernandez <37369676+extraertond@users.noreply.github.com>
2018-05-15 12:20:32 +0200

    Update DietaSemanal.java

--- prCompumatica/src/prCompumatica/DietaSemanal.java
+++ prCompumatica/src/prCompumatica/DietaSemanal.java
@@ -1,5 +1,27 @@
 package prCompumatica;
 
 public class DietaSemanal {
+	
+	Dieta[] semanal = new Dieta[7];
+	int pos = 0; // primera posicion libre
+	
+	public DietaSemanal() {
+		
+	}
+	
+	public Dieta[] getDieta() {
+		return semanal;
+	}
+	
+	public void put (Dieta diet) {
+		if (pos == 7) {
+			System.out.println("error: array de dietas llena");
+		}else {
+			semanal[pos] = diet;
+			pos++;
+		}
+	}
+	
 
 }
+
