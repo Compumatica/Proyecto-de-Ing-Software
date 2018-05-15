34f5c69edbb874e34ff7295459d8cff1d1af1cc8
Manuel López Reviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-05-15 11:12:22 +0200

    Add files via upload

---
+++ prCompumatica/src/prCompumatica/Alergeno.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author Manuel
+ * @see <a href="https://www.makro.es/servicios/alergenos/nueva-ley-alergenos/los-14-alergenos-informar-con-la-nueva-ley">
+ *  Lista de sustancias peligrosas para alergicos según la Ley de Informacion alimentaria vigente</a>
+ */
+public enum Alergeno {
+	LACTOSA,
+	GLUTEN,
+	FRUTOS_DE_CASCARA,
+	MARISCO,
+	HUEVO,
+	CACAHUETE,
+	PESCADO,
+	SOJA,
+	APIO,
+	MOSTAZA,
+	SESAMO,
+	MOLUSCOS,
+	ALTRAMUCES,
+	SO2_Y_SULFITOS;
+}
