2a90ca5a32fbf7288e7a421088991a077f61da10
Manuel López Reviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-05-16 14:09:34 +0200

    Update Dieta.java

--- prCompumatica/src/prCompumatica/Dieta.java
+++ prCompumatica/src/prCompumatica/Dieta.java
@@ -3,37 +3,53 @@ package prCompumatica;
 import java.util.*;
 
 public class Dieta {
-	// EnumMap <Clave, Valor>
-	// Clave es un objeto Enum de Comidas.java (desayuno, almuerzo, merienda, cena)
-	// Valor es un Plato
-	public EnumMap<Comidas, Platos> dieta;
+	
+	private List<Plato>[] platos;
 	
 	public Dieta() {
-		dieta =  new EnumMap<Comidas, Platos>(Comidas.class);
+		platos = new List<>[4];
 	}
 	
-	public void agregaPlato(Platos p, Comidas c) {
-		
-		switch(c) {
-			case DESAYUNO:
-				dieta.put(Comidas.DESAYUNO, p);
-				break;
-			
-			case ALMUERZO:
-				dieta.put(Comidas.ALMUERZO, p);			
-				break;
-				
-			case MERIENDA:
-				dieta.put(Comidas.MERIENDA, p);
-				break;
-				
-			case CENA:
-				dieta.put(Comidas.CENA, p);
-				break;
-				
-			default:
-				break;
-		}
+	// Setters
+	
+	public void setDesayuno(Plato ... platos){
+		set(0, platos);
+	}
+	
+	public void setAlmuerzo(Plato ... platos){
+		set(1, platos);
+	}
+			  
+	public void setMerienda(Plato ... platos){
+		set(2, platos);
+	}
+	
+	public void setCena(Plato ... platos){
+		set(3, platos);
+	}
+	
+	private void set(int index, Plato ... plato){
+		if(platos[index] == null)
+			platos[index] = new ArrayList<>();
+		platos[index].addAll((Collection<? extends Plato>) Arrays.asList(platos));
+	}
+	
+	// Getters
+	
+	public List<Plato> getDesayuno(){
+		return platos[0];
+	}
+	
+	public List<Plato> getAlmuerzo(){
+		return platos[1];
+	}
+	
+	public List<Plato> getMerienda(){
+		return platos[2];
+	}
+	
+	public List<Plato> getCena(){
+		return platos[3];
 	}
 	
 }
