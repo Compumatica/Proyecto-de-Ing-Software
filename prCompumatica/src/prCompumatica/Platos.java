0594c0675a2809cd1aa0e9c44cb9ccef68068952
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-16 13:02:32 +0200

    Add files via upload

--- prCompumatica/src/prCompumatica/Platos.java
+++ prCompumatica/src/prCompumatica/Platos.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class Platos {
 	String nombre;
-	private ArrayList<Alimentos> componentes;
+	private ArrayList<Alimento> componentes;
 	int Grasas, Carbohidratos, Proteinas;
 	
 	// True -> El plato es de almuerzo o cena 
 	// False -> El plato es de cena o merienda
 	boolean franjaAlmuerzo;
 	
-	public Platos(String n, ArrayList<Alimentos> comp){
+	public Platos(String n, ArrayList<Alimento> comp){
 		nombre=n;
 		componentes=comp;
 		
@@ -19,18 +19,18 @@ public class Platos {
 	
 	public Platos(String n) {
 		nombre=n;
-		componentes = new ArrayList<Alimentos>();
+		componentes = new ArrayList<Alimento>();
 	}
 	public Platos() {
 		this("plato_sin_nombre");
 	}
 	// modifiers
-	public void addAlimento(Alimentos a) {
+	public void addAlimento(Alimento a) {
 		componentes.add(a);
 	}
 	// getters
-	public ArrayList<Alimentos> componentes() {
-		ArrayList<Alimentos> clone = new ArrayList<Alimentos>(componentes);
+	public ArrayList<Alimento> componentes() {
+		ArrayList<Alimento> clone = new ArrayList<Alimento>(componentes);
 		return clone;
 	}
 	public String nombre() {
@@ -61,7 +61,7 @@ public class Platos {
 	public void nombre(String n) {
 		nombre=n;
 	}
-	public void componentes(ArrayList<Alimentos> comp) {
+	public void componentes(ArrayList<Alimento> comp) {
 		componentes=comp;
 	}
 	
