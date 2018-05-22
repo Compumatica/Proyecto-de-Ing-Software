ba5da4356f559bb6b929f2e20e356b61cb1bc875
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-22 12:17:32 +0200

    Added method showDetails() to class Dieta.java

--- prCompumatica/src/prCompumatica/Dieta.java
+++ prCompumatica/src/prCompumatica/Dieta.java
@@ -1,8 +1,11 @@
 package prCompumatica;
 
+import java.util.Arrays;
+
 public class Dieta {
 	
 	private Plato[] platos;
+	private int grasas, carbohidratos, proteinas, calorias;
 	
 	public Dieta() {
 		platos = new Plato[4];
@@ -42,6 +45,49 @@ public class Dieta {
 	
 	public Plato getCena(){
 		return platos[3];
+	}
+	
+	public int getGrasas() {
+		for(int i = 0; i < 4; i++) {
+			grasas += platos[i].getGrasas();
+		}
+		return grasas;
+	}
+	
+	public int getCalorias() {
+		for(int i = 0; i < 4; i++) {
+			calorias += platos[i].getCalorias();
+		}
+		return calorias;
+	}
+	
+	public int getCarbohidratos() {
+		for(int i = 0; i < 4; i++) {
+			carbohidratos += platos[i].getCarbohidratos();
+		}
+		return carbohidratos;
+	}
+	
+	public int getProteinas() {
+		for(int i = 0; i < 4; i++) {
+			proteinas += platos[i].getProteinas();
+		}
+		return proteinas;
+	}
+	
+	public String showDetails() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Dieta: \n");
+		sb.append("Calorias totales: \t"+calorias+"\n");
+		sb.append("Proteinas totales: \t"+proteinas+"\n");
+		sb.append("Grasas totales: \t"+grasas+"\n");
+		sb.append("Carbohidratos totales: \t"+carbohidratos+"\n");
+		return sb.toString();
+	}
+	
+	@Override
+	public String toString() {
+		return Arrays.toString(platos);
 	}
 	
 }
