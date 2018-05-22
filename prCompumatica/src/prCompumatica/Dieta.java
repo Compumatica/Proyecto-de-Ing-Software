d051d2fe6387e64d97695fd881bd0ebc0f19f366
Manuel López Reviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-05-22 11:34:34 +0200

    Update Dieta.java

--- prCompumatica/src/prCompumatica/Dieta.java
+++ prCompumatica/src/prCompumatica/Dieta.java
@@ -1,54 +1,46 @@
 package prCompumatica;
 
-import java.util.*;
-
 public class Dieta {
 	
-	private List<Plato>[] platos;
+	private Plato[] platos;
 	
 	public Dieta() {
-		platos = new List<>[4];
+		platos = new Plato[4];
 	}
 	
 	// Setters
 	
-	public void setDesayuno(Plato ... platos){
-		set(0, platos);
+	public void setDesayuno(Plato plato){
+		platos[0] = plato;
 	}
 	
-	public void setAlmuerzo(Plato ... platos){
-		set(1, platos);
+	public void setAlmuerzo(Plato plato){
+		platos[1] = plato;
 	}
 			  
-	public void setMerienda(Plato ... platos){
-		set(2, platos);
+	public void setMerienda(Plato plato){
+		platos[2] = plato;
 	}
 	
-	public void setCena(Plato ... platos){
-		set(3, platos);
-	}
-	
-	private void set(int index, Plato ... plato){
-		if(platos[index] == null)
-			platos[index] = new ArrayList<>();
-		platos[index].addAll((Collection<? extends Plato>) Arrays.asList(platos));
+	public void setCena(Plato plato){
+		platos[3] = plato;
 	}
 	
 	// Getters
 	
-	public List<Plato> getDesayuno(){
+	public Plato getDesayuno(){
 		return platos[0];
 	}
 	
-	public List<Plato> getAlmuerzo(){
+	public Plato getAlmuerzo(){
 		return platos[1];
 	}
 	
-	public List<Plato> getMerienda(){
+	public Plato getMerienda(){
 		return platos[2];
 	}
 	
-	public List<Plato> getCena(){
+	public Plato getCena(){
 		return platos[3];
 	}
 	
