d44c18dd20d5b9c12f8650200d84f72297889e6b
Manuel López Reviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-05-23 14:07:18 +0200

    Update Usuario.java

--- prCompumatica/src/prCompumatica/Usuario.java
+++ prCompumatica/src/prCompumatica/Usuario.java
@@ -1,37 +1,70 @@
 package prCompumatica;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Usuario {
 	private String usuario;
-	private String password;
+	private String contraseña;
 	private String email;
 	
-	public Usuario(String usuario, String password, String email){
+	private Sexo sexo;
+	
+	private List<Alergeno> alergenos;
+	private List<Alimento> alimentosVetados;
+	
+	/**
+	 * 
+	 * @param usuario
+	 * @param contraseña
+	 * @param email
+	 */
+	public Usuario(String usuario, String contraseña, String email){
+		this(usuario, contraseña, email, Sexo.INDETERMINADO);
+	}
+	
+	public Usuario(String usuario, String contraseña, String email, Sexo sexo){
 		this.usuario = usuario;
-		this.password = password;
+		this.contraseña = contraseña;
 		this.email = email;
-		Datos.registraUsuario(usuario, password, email);
+		this.sexo = sexo;
+	}
+	
+	public void setAlergenos(Alergeno...alergenos) {
+		this.alergenos = Arrays.asList(alergenos);
+	}
+	
+	public void vetarAlimentos(Alimento...alimentos) {
+		this.alimentosVetados = Arrays.asList(alimentos);
+	}
+	
+	public List<Alergeno> getAlergenos(){
+		return alergenos;
+	}
+	
+	public List<Alimento> getAlimentosVetados(){
+		return alimentosVetados;
 	}
 	
 	public String consultarDatosPersonales() {
-		return Datos.datosPersonales(usuario);
+		return ""; // dejar asi hasta que tengamos la clase de bd
 	}
 	
 	public String consultarDatosSobreDietas() {
-		return Datos.datosDietas(usuario);
+		return ""; // dejar asi hasta que tengamos la clase de bd
 	}
 	
 	public String modificarDatos() {
-		return Datos.modificaDatosPersonales(usuario);
+		return ""; // dejar asi hasta que tengamos la clase de bd
 	}
 	
 	public void darOpinion(String texto) {
-		Opinion op = new Opinion(texto);
-		//falta implementar un constructor para la clase Datos
-		Datos datos = null;
-		op.mandarOpinion(datos);
+		//Opinion op = new Opinion(texto);
+		//Opinion.mandarOpinion(op);
 	}
 	
 	public String pedirDieta() {
-		return AsistenteDieta.hacerSugerencia(usuario);
+		//return AsistenteDieta.hacerSugerencia(usuario);
+		return "";
 	}
 }
