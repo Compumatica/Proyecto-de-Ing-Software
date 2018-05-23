e6c14d2305bdd6cda450946204074e58add35240
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-23 13:55:54 +0200

    Add files via upload

--- prCompumatica/src/prCompumatica/BaseDeDatos.java
+++ prCompumatica/src/prCompumatica/BaseDeDatos.java
@@ -1,8 +1,12 @@
 package prCompumatica;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class BaseDeDatos implements IBaseDeDatos{
 	
@@ -52,5 +56,53 @@ public class BaseDeDatos implements IBaseDeDatos{
         in.close();
         return res.trim();
 	}
+	
+	private String sendEcho(URL phpURL, Tupla ... pares) {
+		 try {
+	            // open a connection to the site
+	            URLConnection con = phpURL.openConnection();
+	            // activate the output
+	            con.setDoOutput(true);
+	            PrintStream ps = new PrintStream(con.getOutputStream());
+	            // send your parameters to your site
+	            for(Tupla p : pares) {
+	            	ps.print("&" + p.s1() + "=" + p.s2());
+	            }
 
+	            // we have to get the input stream in order to actually send the request
+	            con.getInputStream();
+
+	            // close the print stream
+	            ps.close();
+            } catch (MalformedURLException e1) {
+                e1.printStackTrace();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+	}
+	
+	class Tupla {
+		String t1, t2;
+		public Tupla(String t1, String t2) {
+			this.t1 = t1;
+			this.t2 = t2;
+		}
+		
+		public String s1() {
+			return t1;
+		}
+		
+		public String s2() {
+			return t1;
+		}
+		
+		public void setS1(String s) {
+			t1 = s;
+		}
+		
+		public void setS2(String s) {
+			t2 = s;
+		}
+	}
+	
 }
