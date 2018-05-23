cec419f9a53d968df81047838f3b18f188ee5edd
Antonio Jesús Sánchez Cerván <37370182+Sanchezuma@users.noreply.github.com>
2018-05-23 13:31:04 +0200

    Clase BaseDeDatos e Interfaz IBaseDeDatos
    Clase que se comunica con una Base de Datos mediante PHP

---
+++ prCompumatica/src/prCompumatica/BaseDeDatos.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class BaseDeDatos implements IBaseDeDatos{
+	
+	public final static URL URL_LOGIN = new URL("");
+	public final static URL URL_EMAIL_EXISTS = new URL("");
+	private final static String OK = "0";
+	
+	private Usuario usuario;
+	private boolean existe = false;
+	
+	@Override
+	public void registrarCuenta(String usuario, String clave, String email) {
+		usuario = new Usuario(usuario, clave, email);
+	}
+
+	@Override
+	public void eliminarCuenta(String campo, String clave, boolean campoEsEmail) {
+		
+	}
+
+	@Override
+	public boolean verificarCredenciales(String campo, String clave, boolean campoEsEmail) {
+		return getEcho(URL_LOGIN).equals(OK);	
+	}
+
+	@Override
+	public boolean existeEmail(String email) {
+		return getEcho(URL_EMAIL_EXISTS).equals(OK);
+	}
+
+	@Override
+	public void cambiarEmail(String usuario, String clave, String nuevoEmail) {
+		
+	}
+
+	@Override
+	public void cambiarClave(String usuario, String clave, String nuevaClave) {
+		
+	}
+	
+	private String getEcho(URL phpURL) {
+		String res = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(phpURL.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            res = inputLine;
+        in.close();
+        return res.trim();
+	}
+
+}
