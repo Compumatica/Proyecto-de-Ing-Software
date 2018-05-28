1a76f4c33f8c55d7fbe77bf02a8bb39dae1484db
Manuel López Reviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-05-28 23:49:43 +0200

    Update IBaseDeDatos.java

--- prCompumatica/src/prCompumatica/IBaseDeDatos.java
+++ prCompumatica/src/prCompumatica/IBaseDeDatos.java
@@ -1,22 +1,16 @@
 package prCompumatica;
 public interface IBaseDeDatos {
-	
-	
-	void registrarCuenta(String usuario, String clave, String email);
-	
-	
+	boolean registrarCuenta(String usuario, String clave, String email);
+
 	void eliminarCuenta(String campo, String clave, boolean campoEsEmail);
-	
 
 	boolean verificarCredenciales(String campo, String clave, boolean campoEsEmail);
 	
-	
 	boolean existeEmail(String email);
-
     
 	void cambiarEmail(String usuario, String clave, String nuevoEmail);
-
 	
 	void cambiarClave(String usuario, String clave, String nuevaClave);
 	
+	Usuario usuarioLogueado();
 }
